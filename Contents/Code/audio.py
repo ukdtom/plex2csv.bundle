f4246294cae6ea32450b3fed252855b70e21bcea
Tommy Mikkelsen <tm@orstrand.dk>
2015-08-22 02:23:56 +0200

    V0.0.3.3 released....See the changelog

--- Contents/Code/audio.py
+++ Contents/Code/audio.py
@@ -10,7 +10,7 @@ import misc
 ####################################################################################################
 def getMusicHeader(PrefsLevel):
 	# Simple fields
-	fieldnames = ('Media ID',
+	fieldnames = ('Track ID',
 			'Artist', 
 			'Album',
 			'Title',
@@ -24,6 +24,15 @@ def getMusicHeader(PrefsLevel):
 			'Added',
 			'Updated',
 			)
+	# Extended fields
+	if (PrefsLevel in ['Extended','Extreme', 'Extreme2']):
+		fieldnames = fieldnames + (
+			'Media ID',
+			'bitrate',
+			'audioChannels',
+			'audioCodec',
+			'container',
+			)
 	return fieldnames
 
 ####################################################################################################
@@ -35,14 +44,30 @@ def getAudioInfo(myMedia, myRow, MYHEADER, csvwriter, myMedia2):
 		# Get Basic Info
 		if Prefs['Artist_Level'] in ['Basic', "Extended", "Extreme", "Extreme 2", "Extreme 3"]:
 			myRow = getAudioBasic(myMedia, myRow, myMedia2)
+		if Prefs['Artist_Level'] in ["Extended", "Extreme", "Extreme 2", "Extreme 3"]:
+			myRow = getAudioExtended(myMedia, myRow, myMedia2)
 		return myRow
+
+####################################################################################################
+# This function will return the extended info for Audio
+####################################################################################################
+def getAudioExtended(myMedia, myRow, myMedia2):
+	for track in myMedia2.xpath('//Track'):
+		if track.get('ratingKey') == myRow['Track ID']:
+			myRow['Media ID'] = track.get('ratingKey')
+			for Media in track.xpath('//Media'):
+				myRow['bitrate'] = Media.get('bitrate')
+				myRow['audioChannels'] = Media.get('audioChannels')
+				myRow['audioCodec'] = Media.get('audioCodec')
+				myRow['container'] = Media.get('container')
+	return myRow
 
 ####################################################################################################
 # This function will return the simple info for Audio
 ####################################################################################################
 def getAudioSimple(myMedia, myRow, myMedia2):
 	# Get episode rating key
-	myRow['Media ID'] = misc.GetRegInfo(myMedia2, 'ratingKey')
+	myRow['Track ID'] = misc.GetRegInfo(myMedia2, 'ratingKey')
 	# Get Track title
 	myRow['Title'] = misc.GetRegInfo(myMedia2, 'title')	
 	# Get Artist
@@ -62,4 +87,3 @@ def getAudioBasic(myMedia, myRow, myMedia2):
 	myRow['Updated'] = misc.ConvertDateStamp(misc.GetRegInfo(myMedia2, 'updatedAt', '0'))
 	return myRow
 
-	
--- Contents/Code/consts.py
+++ Contents/Code/consts.py
@@ -4,13 +4,14 @@
 ####################################################################################################
 
 # APP specific stuff
-VERSION = ' V0.0.3.2'
+VERSION = ' V0.0.3.3'
 NAME = 'Plex2csv'
 DESCRIPTION = 'Export Plex libraries to CSV-Files'
 ART = 'art-default.jpg'
 ICON = 'icon-Plex2csv.png'
 PREFIX = '/applications/Plex2csv'
 APPGUID = '7608cf36-742b-11e4-8b39-00089bd210b2'
+PLAYLIST = 'playlist.png'
 
 # How many items we ask for each time
 CONTAINERSIZEMOVIES = 20
